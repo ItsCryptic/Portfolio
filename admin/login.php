8aa03c7721f2f86478d4627ac750270bf45c5863
Kevin D <43621653+ItsCryptic@users.noreply.github.com>
2021-11-17 07:42:11 -0500

    Remove php

--- admin/login.php
+++
@@ -1,9 +0,0 @@
-<?php
-$mysqli = new mysqli("HOST", "USERNAME", "PASSWORD", "DATABASE");
-
-if ($mysqli -> connect_errno) {
-    echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-    exit();
-}
-
-?>
