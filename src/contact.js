0c0e7589c63a9fd2b0ec1e05f376ab0f7ec99b73
Kevin D <43621653+ItsCryptic@users.noreply.github.com>
2021-11-29 11:49:34 -0500

    Remove old files

--- src/contact.js
+++
@@ -1,8 +0,0 @@
-const contact = { 
-    "email": "kevin.diangelis@gmail.com",
-    "twitter": "@ItsCryptic1"
-}
-
-document.getElementById('email').innerHTML = contact.email;
-document.getElementById('twitter').innerHTML = contact.twitter;
-
--- src/easter.js
+++
@@ -1,8 +0,0 @@
-var date = new Date();
-var day = date.getDay();
-var hours = date.getHours();
-var daynumber = date.getDate();
-
-if (month == 3 && daynumber == 17) {
-    document.get
-}
