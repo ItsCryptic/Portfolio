96ca8d16352d1599344f2ef86ad990276774f9d1
Kevin D <43621653+ItsCryptic@users.noreply.github.com>
2021-11-29 21:57:45 -0500

    Add about to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { about } from './src/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ { path: 'about', component: about },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
