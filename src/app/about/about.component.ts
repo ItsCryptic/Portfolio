646848fd3b1ea27ebe85df20ac41ec6b2bbb5aaa
Kevin D <43621653+ItsCryptic@users.noreply.github.com>
2021-12-03 09:16:08 -0500

    Update gitignore & do things

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { DatePipe } from '@angular/common';
-
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
@@ -12,9 +11,12 @@ export class AboutComponent implements OnInit {
   todayString: string = new Date().toDateString();
   todayISOString: string = new Date().toISOString();
   birthdayDate: Date = new Date("June 21 2022");
+  datepipe: DatePipe = new DatePipe('en-US');
+  formattedDate = this.datepipe.transform(this.birthdayDate.valueOf() - this.todayNumber, "(in " + "MM dd hh MM ss)");
 
-  
-
+  function (params:number) {
+    this.formattedDate = this.datepipe.transform(this.birthdayDate.valueOf() - this.todayNumber, "(in " + "MM dd hh MM ss)");
+  }
   constructor() { }
 
   ngOnInit(): void {
--- src/app/src/contact.ts
+++ src/app/src/contact.ts
@@ -3,6 +3,6 @@ const contact = {
     "twitter": "@ItsCryptic1"
 }
 
-document.getElementById("email").innerHTML = contact.email;
-document.getElementById('twitter').innerHTML = contact.twitter;
+/*document.getElementById("email").innerHTML = contact.email;
+document.getElementById('twitter').innerHTML = contact.twitter;*/
 
