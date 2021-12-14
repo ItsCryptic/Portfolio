671f8c5e3f29c241fe6b9ee71075f2a58266cfb0
Kevin D <43621653+ItsCryptic@users.noreply.github.com>
2021-12-14 11:19:18 -0500

    is this what life feels like after you finish something!

---
+++ src/app/src/prideMonth.ts
@@ -0,0 +1,25 @@
+/*var date = new Date();
+var hours = date.getHours();
+var day = date.getDay();
+var seconds = date.getSeconds();
+var minutes = date.getMinutes();
+var month = date.getMonth();
+var pridemonth = document.getElementById("pride-month");
+
+if (month == 11) {
+    //@ts-ignore
+    document.getElementById("pride-month").style.background = `linear-gradient(90deg, #f00, #ff2b00, #f50, #ff8000, #fa0, #ffd500, #ff0, #d4ff00, #af0, #80ff00, #5f0, #2bff00, #0f0, #00ff2b, #0f5, #00ff80, #0fa,#00ffd5, #0ff, #00d4ff, #0af, #007fff, #05f, #002bff, #00f, #2a00ff, #50f, #7f00ff, #a0f, #d400ff, #f0f, #ff00d4, #f0a, #ff0080, #f05, #ff002b, #f00)`;
+    //@ts-ignore
+    document.getElementById("pride-month").style.color = `#0000`;
+    //@ts-ignore
+    document.getElementById("pride-month").style.backgroundClip = `text`;
+    //@ts-ignore
+    document.getElementById("pride-month").style.fontWeight = `bold`;
+    //@ts-ignore
+    document.getElementById("pride-month").style.display = `inline-block`;
+    //@ts-ignore
+    document.getElementById("pride-month").innerHTML = 'Happy Pride Month!';
+    //@ts-ignore
+    document.getElementById("pride-month").style.fontSize = `30px`;
+}*/
+
