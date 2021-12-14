1e944a1555ce47db14b0c621718fdb7ada1dd14b
Kevin D <43621653+ItsCryptic@users.noreply.github.com>
2021-12-14 13:57:28 -0500

    Add academics to router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './home/home.component';
 import { NotFound404Component } from './not-found404/not-found404.component';
 import { AdminComponent } from './admin/admin.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { AcademicsComponent } from './academics/academics.component';
 
 const routes: Routes = [
  { path: '', component: HomeComponent  },
@@ -12,7 +13,8 @@ const routes: Routes = [
  { path: 'admin', component: AdminComponent},
  { path: 'dashboard', component: DashboardComponent },
  { path: '**', component: NotFound404Component },
- { path: 'archived_projects', redirectTo: 'archived_projects/'}
+ { path: 'archived_projects', redirectTo: 'archived_projects/'},
+ { path: 'academics', component: AcademicsComponent }
 ];
 
 @NgModule({
