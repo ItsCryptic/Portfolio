7f159dd840e46858856deb8d22ebc0ec26b1b612
Kevin D <hello@kevind.me>
2022-01-17 00:33:27 -0500

    Make variable names easier to read & follow

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -21,14 +21,14 @@ ngOnInit(): void {
   if (newDate = birthdayDate) {
     var age = age + 1;
   }
-  let age1 = age.toString();
-  let newDate1 = newDate.valueOf();
+  let ageString = age.toString();
+  let newDateNumber = newDate.valueOf();
   //@ts-ignore
-  document.getElementById("age").innerHTML = age1;
+  document.getElementById("age").innerHTML = ageString;
 
   var birthday = setInterval(function() {
       var now = new Date().getTime();
-      var distance = newDate1 - now;
+      var distance = newDateNumber - now;
       var days = Math.floor(distance / (1000 * 60 * 60 * 24));
       var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
       var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
