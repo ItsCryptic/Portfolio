7e9b6c144e07ba161612dfe847b7c7c043d43257
Kevin D <hello@kevind.me>
2022-12-14 13:19:12 +0000

    Preload every page on the server side

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -3,11 +3,31 @@ import { ServerModule } from '@angular/platform-server';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { GpgComponent } from './gpg/gpg.component';
+import { TeamxComponent } from './teamx/teamx.component';
+import { StateComponent } from './state/state.component';
+import { AcademicsComponent } from './academics/academics.component';
+import { AdminComponent } from './admin/admin.component';
+import { ComissionsComponent } from './comissions/comissions.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SchoolHomeComponent } from './school-home/school-home.component';
 
 @NgModule({
   imports: [
     AppModule,
     ServerModule,
+    AboutComponent,
+    HomeComponent,
+    GpgComponent,
+    TeamxComponent,
+    StateComponent,
+    AcademicsComponent,
+    AdminComponent,
+    ComissionsComponent,
+    DashboardComponent,
+    SchoolHomeComponent,
   ],
   bootstrap: [AppComponent],
 })
