3b95a979577de9d20d864010667aa75b6c0c1cd3
Kevin D <hello@kevind.me>
2023-03-02 14:18:07 +0000

    start scraping analytics to eventually be shown

---
+++ src/app/src/analytics.js
@@ -0,0 +1,8 @@
+function AnalyticsAPI() {
+    $(document).ready(function() {
+        $.get("https://analytics.umami.is/share/me7gzLGN3gb0u5P7/kevind.me", function(html){
+            //MetricCard_value__6m_Rf
+            
+        });
+    })
+}
